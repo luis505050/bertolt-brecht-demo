d5e51a62e71063c5d6ab48f708491dc1f50a6bcf
by_nanz <161359614+luis505050@users.noreply.github.com>
2024-11-07 18:05:31 -0500

    Add files via upload

---
+++ JS/scroll_nav.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector("header");
+
+    if (window.scrollY > 0) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
